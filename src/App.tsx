ca4b135602ac7858cef0e0755baa6540af832759
zil130 <55101149+zil130@users.noreply.github.com>
2024-05-30 18:04:59 -0300

    feat: data request

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,46 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
+import ICourse from './types/ICourse';
 
 const App: FC = () => {
+  const [courses, setCourses] = useState<ICourse[]>([]);
+  const [error, setError] = useState<string>('');
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const fetchCourses = async () => {
+    setIsLoading(true);
+    setError('');
+    try {
+      const url = 'https://logiclike.com/docs/courses.json';
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data = await response.json();
+      setCourses(data);
+    } catch (e) {
+      setError((e as Error).message);
+      console.error(e);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchCourses();
+  }, []);
+
+  const tags = [...courses.reduce((acc, course) => {
+    course.tags.forEach(tag => acc.add(tag));
+    return acc;
+  }, new Set<string>())];
+
   return (
-    <div>Hello, LogicLike!</div>
+    <>
+      {isLoading && <div>Loading...</div>}
+      {error && <div>{error}</div>}
+      {courses.length > 0 && console.log(courses)}
+      {courses.length > 0 && console.log(tags)}
+    </>
   );
 };
 
---
+++ src/types/ICourse.ts
@@ -0,0 +1,9 @@
+interface ICourse {
+  name: string;
+  id: string;
+  image: string;
+  bgColor: string;
+  tags: string[];
+}
+
+export default ICourse;
